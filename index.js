f323f9f6ee283a810916d7e552d1dfd7f4fb0366
poison-vine <vine_trigger@live.com>
2022-05-05 17:55:14 -0500

    adds operate function to index.js

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ function divide(x, y){
     return x / y;
 }
 
+function operate(x,y,operator)
+{
+    switch(operator){
+        case 'divide':
+            divide(x,y);
+            break;
+        case 'multiply':
+            multiply(x,y);
+            break;
+        case 'add':
+            add(x,y);
+            break;
+        case 'subtract':
+            subtract(x,y);
+            break;
+    }
+}
+
+
